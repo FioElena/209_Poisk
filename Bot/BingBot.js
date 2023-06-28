633a3d887053f8cbdac3042fb2be3fbe66a4aaf5
Elena <135700554+FioElena@users.noreply.github.com>
2023-06-28 19:57:18 +0300

    Умеет перебирать ссылки на целевом сайте
    С долей вероятности уходит на главную поисковика.
    Если страница целевого сайта не имеет ссылки для выхода, то уходит на главную.

--- Bot/BingBot.js
+++ Bot/BingBot.js
@@ -5,6 +5,7 @@
 // @description  Script for search
 // @author       Fioshkina Elena
 // @match        https://www.bing.com/*
+// @match        https://www.shatura.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -12,22 +13,76 @@
 
 let links = document.links;
 let search = document.getElementsByName("search")[0];
-let keywords = ["Каталог мебели", "Купить мебель", "Магазин мебели"];
+let keywords = [
+    "Каталог мебели", 
+    "Купить мебель", 
+    "Магазин мебели", 
+    "Мебель для прихожей"
+];
 let keyword = keywords[getRandom(0, keywords.length)];
+let bingInput = document.getElementsByName("q") [0];
+let pageNext = document.querySelector(".sb_pagN.sb_pagN_bp.b_widePag.sb_bp");
 
 if (search != undefined) {
-document.getElementsByName("q") [0].value = keyword;
-search.click();
-} else {
-    for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("shatura.com")) {
-        let link = links[i];
-        console.log("Нашел строку " + link);
-        link.click();
-        break;
+    let i = 0;
+    let timerId = setInterval(() => {
+        bingInput.value += keyword[i];
+        i++;
+        if (i == keyword.length) {
+            clearInterval(timerId);
+            search.click();
+        }
+    }, 500)
+
+    } else if (location.hostname == "www.shatura.com") {
+        console.log("Мы на целевом сайте");
+        setInterval(() => {
+            let index = getRandom(0, links.length);
+
+
+            if (getRandom(0, 101) >= 70) {
+                location.href = "https://www.bing.com/";
+            }
+            if (links.length == 0) {
+                location.href = "www.shatura.com"
+            }
+            else if (links[index].href.indexOf("www.shatura.com") != -1) {
+                links[index].click();
+            }
+        }, getRandom(3500, 5500))
+
+    } else {
+        let nextBingPage = true;
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].href.includes("shatura.com")) {
+                let link = links[i];
+                nextBingPage = false;
+                console.log("Нашел строку " + link);
+                setTimeout(() => {
+                    link.click();
+                }, getRandom(3500, 5500))
+                break;
+            }
+        }
+
+        let elementExist = setInterval(() => {
+            let element = document.querySelector(".sb_pagS");
+
+            if (element != null) {
+                if (element.innerText == "5") {
+                    nextBingPage = false;
+                    location.href = "https://www.bing.com/";
+                }
+                clearInterval(elementExist);
+            }
+        }, 150)
+
+        if (nextBingPage) {
+            setTimeout(() => {
+                pageNext.click();
+            }, getRandom(5000, 7000))
+        }
     }
-}
-}
 
 function getRandom(min,max) {
     return Math.floor(Math.random () * (max-min) + min);
